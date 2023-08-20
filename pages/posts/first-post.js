377ae9a3c65833626a4f957568102b815f75f1e2
bengin34 <becaglar3434@gmail.com>
2023-08-20 21:07:33 +0200

    push

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,30 @@
-import Link from 'next/link'
-
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
-    return (
-    <>
-<h1>First Post</h1>
-<h2>
-    <Link href="/"> Back to home </Link>
-</h2>
-    </>
-    )
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+      src="https://connect.facebook.net/en_US/sdk.js"
+      strategy="lazyOnload"
+      onLoad={()=> 
+      console.log('SDK Loaded!')
+      }
+
+      />
+
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/"> Back to home </Link>
+      </h2>
+
+
+    </Layout>
+  );
 }
